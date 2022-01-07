a76a8564c59d5e30afd14a5509fe0c6d9e160f48
Ericson Marques <ericsonmarques@ericsons-mbp.kpn>
2022-01-07 17:50:31 +0100

    first

--- streamlit_scatterplot_selection/__init__.py
+++ streamlit_scatterplot_selection/__init__.py
@@ -1,3 +1,42 @@
 import json
 import os
 import random
+
+import plotly.utils
+import streamlit.components.v1 as components
+
+_RELEASE = False
+
+if not _RELEASE:
+    _component_func = components.declare_component(
+        "streamlit_scatterplot_selection",
+        url="http://localhost:3001",
+    )
+else:
+    parent_dir = os.path.dirname(os.path.abspath(__file__))
+    build_dir = os.path.join(parent_dir, "frontend/build")
+    _component_func = components.declare_component(
+        "streamlit_scatterplot_selection", path=build_dir)
+
+
+def st_scatterplot(fig, key="st_plotly"):
+    spec = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+    selected_points = _component_func(spec=spec, default=[], key=key)
+    return selected_points
+
+
+if not _RELEASE:
+    import random
+    import plotly.express as px
+    import streamlit as st
+
+    @st.cache
+    def random_data():
+        return random.sample(range(100), 50), random.sample(range(100), 50)
+
+    st.subheader("Plotly interactive scatterplot")
+    x, y = random_data()
+    fig = px.scatter(x=x, y=y, title="My fancy plot")
+    v = st_scatterplot(fig)
+    st.write(fig)
+    st.write(v)
