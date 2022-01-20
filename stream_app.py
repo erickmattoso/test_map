8bdff38431a1769f42988dbbf41966f990befef9
Ericson Marques <ericsonmarques@ericsons-mbp.kpn>
2022-01-20 18:28:56 +0100

    update

--- stream_app.py
+++ stream_app.py
@@ -9,6 +9,7 @@ import plotly.graph_objects as go
 date_1 = datetime.datetime.strptime("22/01/2022", "%d/%m/%Y")
 
 loc_button = Button(label="Get Location")
+
 loc_button.js_on_event("button_click", CustomJS(code="""
     navigator.geolocation.getCurrentPosition(
         (loc) => {
@@ -16,23 +17,20 @@ loc_button.js_on_event("button_click", CustomJS(code="""
         }
     )
     """))
+
 result = streamlit_bokeh_events(
     loc_button,
     events="GET_LOCATION",
     key="get_location",
-    # refresh_on_update=True,
     override_height=40,
     debounce_time=0)
 
 csv_csv = pd.read_csv('csv_csv.csv')
 df = pd.read_csv('df.csv')
-dias = 9
-lugares_dia = 5
-n_places = dias * lugares_dia
-other = n_places*lugares_dia
-MYHOME = [["Home", 48.8841155, 2.3465937]]
+
+MYHOME = [["Home", 48.8841155, 2.3465937, 0]]
 home = pd.DataFrame(MYHOME)
-home.columns = ["title", "lat", "lng"]
+home.columns = ["title", "lat", "lng", "price"]
 
 
 def plot_figure(my_df, ranges):
@@ -83,8 +81,7 @@ def plot_figure(my_df, ranges):
     return fig
 
 
-values = st.slider('Select a range of values',
-                   n_places, len(df), (n_places, other))
+values = st.slider('Select a range of values', 0, len(df), (0, len(csv_csv)))
 
 fig = plot_figure(csv_csv, values)
 
@@ -96,7 +93,7 @@ csv_csv_copy = csv_csv.copy()
 csv_csv_copy = csv_csv_copy[csv_csv_copy['title'] != 'Home']
 csv_csv_copy = csv_csv_copy[csv_csv_copy['title'] != 'Disney']
 csv_csv_copy = csv_csv_copy[csv_csv_copy['title'] != 'Palace of Versailles']
-# csv_csv_copy = csv_csv_copy.reset_index(drop=True)
+csv_csv_copy = csv_csv_copy.reset_index(drop=True)
 
 i = j = 0
 fig = plot_figure(csv_csv.head(1), values)
@@ -115,20 +112,76 @@ for n in range(7):
         }))
 row2.plotly_chart(fig, use_container_width=True)
 
+# i = j = 0
+# for n in range(7):
+#     i = j
+#     j = i+5
+#     dias_percorridos = pd.concat([
+#         home,
+#         csv_csv_copy.iloc[i:j],
+#         home
+#     ])
+#     fig = plot_figure(dias_percorridos, values)
+#     end_date = (date_1 + datetime.timedelta(days=n)).strftime("%d/%m/%Y")
+#     st.write(f"Visitar no dia {n+1} - {end_date}")
+#     st.write(dias_percorridos[['title', 'price']])
+#     st.markdown(list(dias_percorridos.title))
+#     st.plotly_chart(fig, use_container_width=True)
+#     st.markdown(list(dias_percorridos.index))
 
-i = j = 0
 
-for n in range(7):
-    i = j
-    j = i+5
-    dias_percorridos = pd.concat([
-        home,
-        csv_csv_copy.iloc[i:j],
-        home
-    ])  # .reset_index(drop=True)
-    fig = plot_figure(dias_percorridos, values)
-    end_date = (date_1 + datetime.timedelta(days=n)).strftime("%d/%m/%Y")
-    st.write(f"Visitar no dia {n+1} - {end_date}")
-    st.write(dias_percorridos[['title', 'duration', 'price']])
-    st.write(dias_percorridos.index)
-    st.plotly_chart(fig, use_container_width=True)
+dias_percorridos = pd.concat([csv_csv_copy, home]).reset_index(drop=True)
+
+st.title("22/01/2022")
+df_plot = dias_percorridos.iloc[[34, 10, 11, 12, 13, 14, 34]]
+fig = plot_figure(df_plot, values)
+st.write(df_plot[['title']])
+st.plotly_chart(fig, use_container_width=True)
+
+st.title("23/01/2022")
+df_plot = dias_percorridos.iloc[[34, 20, 21, 22, 23, 24, 34]]
+fig = plot_figure(df_plot, values)
+st.write(df_plot[['title']])
+st.plotly_chart(fig, use_container_width=True)
+
+st.title("24/01/2022")
+df_plot = dias_percorridos.iloc[[34, 30, 31, 32, 33, 34]]
+fig = plot_figure(df_plot, values)
+st.write(df_plot[['title']])
+st.plotly_chart(fig, use_container_width=True)
+
+st.title("25/01/2022")
+df_plot = dias_percorridos.iloc[[34, 15, 16, 17, 18, 19, 34]]
+fig = plot_figure(df_plot, values)
+st.write(df_plot[['title']])
+st.plotly_chart(fig, use_container_width=True)
+
+st.title("26/01/2022")
+df_plot = dias_percorridos.iloc[[34, 5, 6, 7, 8, 9, 34]]
+fig = plot_figure(df_plot, values)
+st.write(df_plot[['title']])
+st.plotly_chart(fig, use_container_width=True)
+
+st.title("27/01/2022")
+df_plot = dias_percorridos.iloc[[34, 25, 26, 27, 28, 29, 34]]
+fig = plot_figure(df_plot, values)
+st.write(df_plot[['title']])
+st.plotly_chart(fig, use_container_width=True)
+
+st.title("28/01/2022")
+df_plot = csv_csv.iloc[[0, 4, 0]]
+fig = plot_figure(df_plot, values)
+st.write(df_plot[['title']])
+st.plotly_chart(fig, use_container_width=True)
+
+st.title("29/01/2022")
+df_plot = csv_csv.iloc[[0, 17, 0]]
+fig = plot_figure(df_plot, values)
+st.write(df_plot[['title']])
+st.plotly_chart(fig, use_container_width=True)
+
+st.title("30/01/2022")
+df_plot = dias_percorridos.iloc[[34, 0, 1, 2, 3, 4, 34]]
+fig = plot_figure(df_plot, values)
+st.write(df_plot[['title']])
+st.plotly_chart(fig, use_container_width=True)
